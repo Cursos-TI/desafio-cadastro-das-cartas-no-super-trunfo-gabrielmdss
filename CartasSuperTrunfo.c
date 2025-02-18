097125ff9dc2609c073fe295b1ce1650b7fe89df
Gabriel Mendes <109549715+gabrielmdss@users.noreply.github.com>
2025-02-18 20:39:10 +0000

    feat:

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,131 +1,87 @@
 #include <stdio.h>
-// Desafio Super Trunfo - Países
-int main()
-{
-    int populacaoA;
-    int areaA;
-    int pibA;
-    int quantidadePontosTuristicosA;
-    int densidadePopulacionalA;
-    int pibPerCapitaA;
-    int populacaoB;
-    int areaB;
-    int pibB;
-    int quantidadePontosTuristicosB;
-    int densidadePopulacionalB;
-    int pibPerCapitaB;
-    printf("Bem vindo ao Desafio Super Trunfo - Nível Novato\n");
-    printf("Por favor, insira as informações da cidade\n");
+#include <string.h>
 
-    printf("Insira a População da cidade A:\n");
-    scanf("%d", &populacaoA);
-    printf("Insira a População da cidade B:\n");
-    scanf("%d", &populacaoB);
+typedef struct {
+    char nome[50];
+    int populacao;
+    int area;
+    int pib;
+    int pontosTuristicos;
+} Carta;
 
-    printf("Insira a Área da cidade A:\n");
-    scanf("%d", &areaA);
-    printf("Insira a Área da cidade B:\n");
-    scanf("%d", &areaB);
+void cadastrarCarta(Carta *carta) {
+    printf("Digite o nome do país: ");
+    scanf(" %[^\n]", carta->nome); // Lê nome com espaços
 
-    printf("Insira o PIB da cidadeA:\n");
-    scanf("%d", &pibA);
-    printf("Insira o PIB da cidadeB:\n");
-    scanf("%d", &pibB);
+    printf("Digite a população: ");
+    scanf("%d", &carta->populacao);
 
-    printf("Insira a quantidade de pontos turísticos da cidade A:\n");
-    scanf("%d", &quantidadePontosTuristicosA);
-    printf("Insira a quantidade de pontos turísticos da cidade B:\n");
-    scanf("%d", &quantidadePontosTuristicosB);
+    printf("Digite a área em km²: ");
+    scanf("%d", &carta->area);
 
-    densidadePopulacionalA = populacaoA / areaA;
-    pibPerCapitaA = pibA / populacaoA;
-    densidadePopulacionalB = populacaoB / areaB;
-    pibPerCapitaB = pibB / populacaoB;
+    printf("Digite o PIB em milhões: ");
+    scanf("%d", &carta->pib);
 
-    int superPoderA = densidadePopulacionalA + areaA + pibA + pibPerCapitaA + quantidadePontosTuristicosA;
-    int superPoderB = densidadePopulacionalB + areaB + pibB + pibPerCapitaB + quantidadePontosTuristicosB;
+    printf("Digite a quantidade de pontos turísticos: ");
+    scanf("%d", &carta->pontosTuristicos);
+}
 
-    if (populacaoA > populacaoB)
-    {
-        printf("A Carta A vence a Carta B com sua população de $d", populacaoA);
-    };
-    if (populacaoA > populacaoB)
-    {
-        printf("A Carta B vence a Carta A com sua população de $d", populacaoB);
-    };
+void compararCartas(Carta cartaA, Carta cartaB) {
+    int escolha1, escolha2;
 
-    if (densidadePopulacionalA < densidadePopulacionalB)
-    {
-        printf("A Carta A vence a Carta B com sua densidade populacional de %d\n", densidadePopulacionalA);
-    }
-    else if (densidadePopulacionalB < densidadePopulacionalA)
-    {
-        printf("A Carta B vence a Carta A com sua densidade populacional de %d\n", densidadePopulacionalB);
-    }
-    else
-    {
-        printf("As cartas têm a mesma densidade populacional de %d\n", densidadePopulacionalA);
+    printf("\nEscolha o primeiro atributo para comparar:\n");
+    printf("1 - População\n2 - Área\n3 - PIB\n4 - Pontos Turísticos\n");
+    scanf("%d", &escolha1);
+
+    do {
+        printf("\nEscolha o segundo atributo para comparar (diferente do primeiro):\n");
+        printf("1 - População\n2 - Área\n3 - PIB\n4 - Pontos Turísticos\n");
+        scanf("%d", &escolha2);
+    } while (escolha2 == escolha1);
+
+    int valorA1, valorA2, valorB1, valorB2;
+
+    switch (escolha1) {
+        case 1: valorA1 = cartaA.populacao; valorB1 = cartaB.populacao; break;
+        case 2: valorA1 = cartaA.area; valorB1 = cartaB.area; break;
+        case 3: valorA1 = cartaA.pib; valorB1 = cartaB.pib; break;
+        case 4: valorA1 = cartaA.pontosTuristicos; valorB1 = cartaB.pontosTuristicos; break;
     }
 
-    if (areaA > areaB)
-    {
-        printf("A Carta A vence a Carta B com sua área de %d Km²\n", areaA);
-    }
-    else if (areaB > areaA)
-    {
-        printf("A Carta B vence a Carta A com sua área de %d Km²\n", areaB);
-    }
-    else
-    {
-        printf("As cartas têm a mesma área de %d Km²\n", areaA);
+    switch (escolha2) {
+        case 1: valorA2 = cartaA.populacao; valorB2 = cartaB.populacao; break;
+        case 2: valorA2 = cartaA.area; valorB2 = cartaB.area; break;
+        case 3: valorA2 = cartaA.pib; valorB2 = cartaB.pib; break;
+        case 4: valorA2 = cartaA.pontosTuristicos; valorB2 = cartaB.pontosTuristicos; break;
     }
 
-    if (pibA > pibB)
-    {
-        printf("A Carta A vence a Carta B com seu PIB de R$ %d\n", pibA);
-    }
-    else if (pibB > pibA)
-    {
-        printf("A Carta B vence a Carta A com seu PIB de R$ %d\n", pibB);
-    }
-    else
-    {
-        printf("As cartas têm o mesmo PIB de R$ %d\n", pibA);
-    }
+    int somaA = valorA1 + valorA2;
+    int somaB = valorB1 + valorB2;
 
-    if (pibPerCapitaA > pibPerCapitaB)
-    {
-        printf("A Carta A vence a Carta B com seu PIB per capita de R$ %d\n", pibPerCapitaA);
-    }
-    else if (pibPerCapitaB > pibPerCapitaA)
-    {
-        printf("A Carta B vence a Carta A com seu PIB per capita de R$ %d\n", pibPerCapitaB);
-    }
-    else
-    {
-        printf("As cartas têm o mesmo PIB per capita de R$ %d\n", pibPerCapitaA);
-    }
+    printf("\nResultado da Comparação:\n");
+    printf("%s: %d + %d = %d\n", cartaA.nome, valorA1, valorA2, somaA);
+    printf("%s: %d + %d = %d\n", cartaB.nome, valorB1, valorB2, somaB);
 
-    if (quantidadePontosTuristicosA > quantidadePontosTuristicosB)
-    {
-        printf("A Carta A vence a Carta B com %d pontos turísticos\n", quantidadePontosTuristicosA);
-    }
-    else if (quantidadePontosTuristicosB > quantidadePontosTuristicosA)
-    {
-        printf("A Carta B vence a Carta A com %d pontos turísticos\n", quantidadePontosTuristicosB);
-    }
-    else
-    {
-        printf("As cartas têm a mesma quantidade de pontos turísticos: %d\n", quantidadePontosTuristicosA);
-    }
-
-    if (superPoderA > superPoderB) {
-        printf("A Carta A vence a Carta B pelo Super Poder!\n");
-    } else if (superPoderB > superPoderA) {
-        printf("A Carta B vence a Carta A pelo Super Poder!\n");
+    if (somaA > somaB) {
+        printf("\nVencedor: %s!\n", cartaA.nome);
+    } else if (somaB > somaA) {
+        printf("\nVencedor: %s!\n", cartaB.nome);
     } else {
-        printf("As cartas têm o mesmo Super Poder! Empate!\n");
+        printf("\nEmpate!\n");
     }
+}
+
+int main() {
+    Carta cartaA, cartaB;
+
+    printf("Cadastro da Carta A:\n");
+    cadastrarCarta(&cartaA);
+
+    printf("\nCadastro da Carta B:\n");
+    cadastrarCarta(&cartaB);
+
+    printf("\nBem-vindo ao Super Trunfo de Países!\n");
+    compararCartas(cartaA, cartaB);
 
     return 0;
 }
